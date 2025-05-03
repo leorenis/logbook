b2ae19d441b6a35849d0279d232647007937384a
leorenis <leorenis@gmail.com>
2025-05-03 12:14:53 -0300

    [Git] Certification Exam Training Process. ZCE

--- languages/php/zce/book_zce/capitulo06/wrappers/ssh.php
+++ languages/php/zce/book_zce/capitulo06/wrappers/ssh.php
@@ -1,7 +1,7 @@
 <?php
 // Com o wrapper ssh2:// , é possível se conectar a servidores utilizando o protocolo SSH de uma maneira simples e elegante. Veja um exemplo de como acessar o conteúdo de um arquivo:
-$ssh = ssh2_connect('192.168.1.106');
-ssh2_auth_password($ssh, 'zend', '123456');
+$ssh = ssh2_connect('SOME_IP');
+ssh2_auth_password($ssh, 'zend', '{SOME_CREDENTIAL}');
 $ftp = ssh2_sftp($ssh);
 $arquivo = fopen("ssh2.sftp://$ftp/foo/bar/teste.txt", 'r');
 
